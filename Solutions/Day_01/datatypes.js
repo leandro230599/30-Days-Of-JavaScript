fcb09281a20d1857608dbc5e5907b6ae0ade8193
Leandro Lopez <leandro230599@outlook.com>
2023-01-20 21:02:03 -0300

    Solutions day 1 complete

---
+++ Solutions/Day_01/datatypes.js
@@ -0,0 +1,9 @@
+let string = "HOLA";
+let bool = true;
+let unde = undefined;
+let nulo = null;
+
+console.log(typeof(string))
+console.log(typeof(bool))
+console.log(typeof(unde))
+console.log(typeof(nulo))
---
+++ Solutions/Day_01/variable.js
@@ -0,0 +1,30 @@
+let string = "HOLA";
+let bool = true;
+let unde = undefined;
+let nulo = null;
+
+let sinvalor1;
+let sinvalor2;
+let sinvalor3;
+let sinvalor4;
+
+let convalor1 = 1;
+let convalor2 = 2;
+let convalor3 = 3;
+let convalor4 = 4;
+
+console.log(sinvalor1)
+console.log(convalor1)
+
+let nombre1 = "Leandro"
+let apellido1 = "Lopez"
+let estado1 = "Soltero"
+let pais1 = "Argentina"
+let edad1 = 23
+
+let nombre2 = "Leandro", apellido2 = "Lopez", estado2 = "Soltero", pais2 = "Argentina", edad = 23;
+
+let myAge = 23, youAge = 30;
+
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${youAge} years old`)
